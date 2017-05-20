d39e49d4145e68c0438bfbfa4d4f34bd6315dfc2
Kendra Kleber <league@Mac-mini-2.local>
2017-05-20 12:09:47 -0700

    Raccoon, Drum Kit

--- src/Racoon.java
+++ src/Racoon.java
@@ -2,6 +2,8 @@ import javax.swing.JOptionPane;
 
 public class Racoon {
 	private int trash;
+	private int patches;
+	private int organs;
 
 	public Racoon(int trash) {
 		this.trash = trash;
@@ -17,14 +19,39 @@ public class Racoon {
 		trash = numTrash;
 	}
 
-	public int getTrash(){
-		System.out.println(trash);
+	public void setPatches(int numPatches){
+		patches = numPatches;
 	}
-
+	public int getTrash(){
+		System.out.println("Your racoon now has " + trash + " pieces of trash.");
+		return trash;
+	}
+	public int getPatches(){
+		System.out.println("Your racoon now has " + patches + " patches.");
+		return patches;
+	}
+	public void setOrgans(int numOrgans){
+		organs = numOrgans;
+		if (numOrgans>3){
+			System.out.println("Your racoon still has " + numOrgans + " organs! They're not roadkill yet...");
+		}
+		else{
+			System.out.println("Whoops... your racoon is dead.");
+		}
+	}
 	public static void main(String[] args) {
-		Racoon Marg = new Racoon(5);
+	/*	Racoon Marg = new Racoon(5);
 		Marg.addTrash(2);
-		Marg.setTrash(7);
-
+		Marg.setTrash(1);
+		Marg.getTrash();
+		Marg.setPatches(4);
+		Marg.getPatches();
+		Marg.setOrgans(3);
+*/
+		Racoon Rodger=new Racoon(10);
+		Rodger.setPatches(3);
+		Rodger.setTrash(13);
+		Rodger.setOrgans(6);
+		Rodger.getTrash();
 	}
 }
